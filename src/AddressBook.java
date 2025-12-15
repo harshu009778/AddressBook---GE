e24205999f963e7323340a1c66da06f47beafd17
harshu009778 <harshu.19791@gmail.com>
2025-12-15 13:44:14 +0530

    Added : Update Contact Details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,4 +20,29 @@ public class AddressBook {
         }
     }
 
+    // ---- UC3 : Edit contact ---------
+    public void editContacts(String name, String newData, int number){
+        boolean flag = true;
+        for(Contact i : contacts){
+            if(i.getFirstName().equalsIgnoreCase(name)){
+                flag = false;
+                if(number == 1) i.setFirstName(newData);
+                else if(number == 2) i.setLastName(newData);
+                else if(number == 3) i.setAddress(newData);
+                else if(number == 4) i.setCity(newData);
+                else if(number == 5) i.setState(newData);
+                else if(number == 6) i.setZipCode(newData);
+                else if(number == 7) i.setPhoneNumber(newData);
+                else if(number == 8) i.setEmail(newData);
+            }
+        }
+
+        if(flag == true){
+            System.out.println("Contact not Found ");
+        }
+        else {
+            System.out.println("Contact details with name " + name + " is updated. " );
+        }
+    }
+
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -12,7 +12,8 @@ public class AddressBookMain {
             System.out.println("Address Book Menu:");
             System.out.println("1. Add Contact");
             System.out.println("2. Display Contacts");
-            System.out.println("3. Exit");
+            System.out.println("3. Edit Contacts");
+            System.out.println("4. Exit");
             System.out.print("Choose an option: ");
 
             int choice = in.nextInt();
@@ -51,6 +52,89 @@ public class AddressBookMain {
                     addressBook.displayContacts();
                     break;
                 case 3:
+                    System.out.println("Enter the contact name :");
+                    String name = in.nextLine();
+
+                    boolean flag = true;
+
+                    while(flag){
+                        System.out.println("What do you want to update next ? ");
+                        System.out.println("1. First Name");
+                        System.out.println("2. Last Name ");
+                        System.out.println("3. Address ");
+                        System.out.println("4. City ");
+                        System.out.println("5. State ");
+                        System.out.println("6. zip code");
+                        System.out.println("7. Phone Number");
+                        System.out.println("8. Email ");
+                        System.out.println("9. Exit");
+                        System.out.print("Choose an option: ");
+
+                        int ch = in.nextInt();
+                        in.nextLine();
+
+                        switch (ch){
+                            case 1:
+                                System.out.print("Enter New First name: ");
+                                String newfirstName = in.nextLine();
+
+                                addressBook.editContacts(name, newfirstName,1);
+                                break;
+                            case 2:
+                                System.out.print("Enter New Last name: ");
+                                String newLastName = in.nextLine();
+
+                                addressBook.editContacts(name, newLastName,2);
+                                break;
+                            case 3:
+                                System.out.print("Enter New Address : ");
+                                String newAddress = in.nextLine();
+
+                                addressBook.editContacts(name, newAddress,3);
+                                break;
+                            case 4:
+                                System.out.print("Enter New City: ");
+                                String newCity = in.nextLine();
+
+                                addressBook.editContacts(name, newCity,4);
+                                break;
+                            case 5:
+                                System.out.print("Enter New State: ");
+                                String newState = in.nextLine();
+
+                                addressBook.editContacts(name, newState,5);
+                                break;
+                            case 6:
+                                System.out.print("Enter New ZipCode: ");
+                                String newZipCode = in.nextLine();
+
+                                addressBook.editContacts(name, newZipCode,6);
+                                break;
+                            case 7:
+                                System.out.print("Enter New Phone number : ");
+                                String newNo = in.nextLine();
+
+                                addressBook.editContacts(name, newNo,7);
+                                break;
+                            case 8:
+                                System.out.print("Enter New Email : ");
+                                String newEmail = in.nextLine();
+
+                                addressBook.editContacts(name, newEmail,8);
+                                break;
+                            case 9:
+                                flag = false;
+                                System.out.println("Exiting. ");
+                                break;
+                            default:
+                                System.out.println("Invalid option. Please try again.");
+
+
+
+                        }
+                    }
+                    break;
+                case 4:
                     running = false;
                     System.out.println("Exiting Address Book.");
                     break;
