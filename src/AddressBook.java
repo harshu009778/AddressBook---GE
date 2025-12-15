6a22234d2d1abb96ed55426b8465b99b7bd44682
harshu009778 <harshu.19791@gmail.com>
2025-12-15 11:56:37 +0530

    Added : addded new Contact to Address Book

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -3,4 +3,21 @@ import java.util.ArrayList;
 public class AddressBook {
     ArrayList<Contact> contacts = new ArrayList<>();
 
+    // ------ UC2 - Ability to add a new Contact to Address Book -------
+    public void addContact(Contact contact){
+        contacts.add(contact);
+        System.out.println("Contact added");
+
+    }
+
+    public void displayContacts(){
+        if(contacts.isEmpty()){
+            System.out.println("No contacts available.");
+        } else {
+            for (Contact i : contacts) {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -6,7 +6,59 @@ public class AddressBookMain {
 
         AddressBook addressBook = new AddressBook();
 
+        Boolean running  = true;
 
+        while(running){
+            System.out.println("Address Book Menu:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display Contacts");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+
+            int choice = in.nextInt();
+            in.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter First name: ");
+                    String firstName = in.nextLine();
+
+                    System.out.print("Enter Last name: ");
+                    String lastName = in.nextLine();
+
+                    System.out.print("Enter phone number: ");
+                    String phoneNumber = in.nextLine();
+
+                    System.out.print("Enter email address: ");
+                    String emailAddress = in.nextLine();
+
+                    System.out.println("Enter address details:");
+                    String address = in.nextLine();
+
+                    System.out.println("Enter City:");
+                    String city = in.nextLine();
+
+                    System.out.println("Enter State:");
+                    String state = in.nextLine();
+
+                    System.out.println("Enter Zip Code:");
+                    String zipCode = in.nextLine();
+
+                    Contact c1 = new Contact(firstName, lastName, phoneNumber, emailAddress, address, city, state, zipCode);
+                    addressBook.addContact(c1);
+                    break;
+                case 2:
+                    addressBook.displayContacts();
+                    break;
+                case 3:
+                    running = false;
+                    System.out.println("Exiting Address Book.");
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+
+            }
+        }
 
     }
 }
