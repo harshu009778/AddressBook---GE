ee504bb5474f21f2316e3607137895197cf91ab7
harshu009778 <harshu.19791@gmail.com>
2025-12-15 11:42:01 +0530

    AddressBook

---
+++ src/AddressBookMain.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        AddressBook addressBook = new AddressBook();
+
+
+
+    }
+}
